6ec51c3fb7494d9eb869d304b39e520f70d83829
Mykhaylo Kuzmin <xxxxxxxxx@gmail.com>
2023-06-02 14:32:32 -0400

    Menu & Header in seperate files

---
+++ js/script.js
@@ -0,0 +1,20 @@
+const menu = document.getElementById('menu');
+const header = document.getElementById('header');
+
+
+
+
+// Import of Menu and Header
+fetch('menu.html')
+.then(response => response.text())
+.then(data => {
+    menu.innerHTML = data;
+})
+.catch(error => console.error(error));
+
+fetch('header.html')
+.then(response => response.text())
+.then(data => {
+    header.innerHTML = data;
+})
+.catch(error => console.error(error));
